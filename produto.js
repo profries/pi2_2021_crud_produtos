e5ab7509e49c4a0f99011ee8bccaf04e0a144780
Luis <luisries.zeta@gmail.com>
2021-03-23 21:42:10 -0300

    Cadastro de produtos

---
+++ produto.js
@@ -0,0 +1,9 @@
+module.exports = class Produto {
+    constructor(codigo, nome, preco) {
+        this.codigo = codigo;        
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+
+// module.exports = Produto;
